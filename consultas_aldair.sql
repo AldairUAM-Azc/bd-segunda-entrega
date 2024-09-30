a3f924c8307ec7043ea6d593a97c377d35c81f35
AldairUAM-Azc <108377001+AldairUAM-Azc@users.noreply.github.com>
2024-09-30 06:23:16 -0600

    query contenido de los combos

--- consultas_aldair.sql
+++ consultas_aldair.sql
@@ -1,4 +1,4 @@
--- 1. Se necesita un ranking de los empleados con mas ventas en la dulceria en el mes
+-- 1. Se necesita un ranking de los empleados con mas ventas en la dulceria en el mes.
 SELECT 
   e.id AS empleado_id, 
   e.nombre, 
@@ -11,3 +11,16 @@ WHERE
   MONTH(v.creada_en) = MONTH(CURDATE()) -- todas las ventas son del mes de septiembre 09
 GROUP BY e.id
 ORDER BY total_venta DESC;
+
+
+-- 2. Quiero saber que snacks estan incluidas en cada combo.
+SELECT
+    c.nombre AS combo_nombre,
+    s.nombre AS snack_nombre,
+    s.tamanio AS snack_tamanio,
+    cs.quantity AS cantidad
+FROM combo c
+  JOIN combo_snack cs ON c.id = cs.id_combo
+  JOIN snack s ON cs.id_snack = s.id
+ORDER BY
+    c.nombre, s.nombre;
