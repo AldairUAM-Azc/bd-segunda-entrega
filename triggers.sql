86c9956b60f29f52077e8f72b767c01d0c872010
Jose Vicente <al2212002118@azc.uam.mx>
2024-09-30 02:20:45 -0600

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,15 @@
+/*Bitabora*/
+CREATE TABLE bitacora (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_operacion VARCHAR(20) NOT NULL,
+    tabla_afectada VARCHAR(50) NOT NULL,
+    numero_registro INT NOT NULL,
+    valores_anteriores MEDIUMTEXT,
+    valores_nuevos MEDIUMTEXT,
+    usuario_aplicacion VARCHAR(50) NOT NULL,
+    fecha DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 -- Triggers  Hernández Aguilar Jonathan Abraham 2182004371
 
 DELIMITER //
@@ -18,3 +30,26 @@ BEGIN
 END;
 
 DELIMITER ;
+
+/*José Vicente López López*/
+-- Trigger Agregar Venta
+DELIMITER //
+CREATE TRIGGER insertar_venta
+AFTER INSERT ON venta
+FOR EACH ROW
+BEGIN
+    -- Registrar la operación en la tabla de bitácora para la venta
+    INSERT INTO bitacora (tipo_operacion, tabla_afectada, numero_registro, valores_anteriores, valores_nuevos, usuario_aplicacion)
+    VALUES ('INSERT', 'venta', NEW.id, '',
+            CONCAT('ID Empleado: ', NEW.id_empleado, '; ID Socio: ', NEW.id_socio, '; Total: ', NEW.total), 
+            USER());
+
+END //
+DELIMITER ;
+
+/* -- Si guarda
+INSERT INTO venta (id_empleado, id_socio, metodo_pago, area, total, creada_en) VALUES
+(1, 5, 'credito', 'taquilla', 230, '2024-09-30 20:30:15');
+
+SELECT * FROM bitacora;
+*/
